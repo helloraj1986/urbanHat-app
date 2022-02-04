024c958cbe7e039ba61db81d5ee1eced8bf0f68f
Rajeev Kumar Mohanty <rajeev.mohanty@live.com>
2022-02-04 19:24:33 +1100

    refactored some warnings

--- src/components/categorySection/trending/trendingCard/TrendingCard.jsx
+++ src/components/categorySection/trending/trendingCard/TrendingCard.jsx
@@ -5,14 +5,10 @@ import FavoriteIcon from '@material-ui/icons/Favorite'
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined'
 import ShareIcon from '@material-ui/icons/Share'
 import { Link } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { setProductHeader } from '../../../../redux/actions/ProductAction'
 
 const TrendingCard = ({ cardData }) => {
 	const classes = useStyles()
-	const { id, category, title, price, description, image } = cardData
-	const dispatch = useDispatch()
-	//component={Link} to={`/products/${cardData.id}`}
+	const { price, image } = cardData
 	return (
 		<Card raised variant='outlined'>
 			<CardActionArea
